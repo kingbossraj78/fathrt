e84a489c6ec0836357ce35b1ceefc01f2092f834
kingbossraj78 <kingbossraj786@gmail.com>
2025-07-11 14:03:25 +0530

    Add files via upload

---
+++ src/Data/data.js
@@ -0,0 +1,345 @@
+
+
+export const data = [
+    {
+        id: 1,
+        type: 'product',
+        title: "Johnnie Walker Black Label Blended Scotch Whisky",
+        image: "https://i.ibb.co/NnJFLr3C/1750956143-9bd5dbe4b9f079b38e441c6f9fb16695-1.jpg",
+        discount: "77% Off",
+        price: "₹1846",
+        additionalPrice: "₹199.00",
+        sliderImages: [
+            "https://i.ibb.co/NnJFLr3C/1750956143-9bd5dbe4b9f079b38e441c6f9fb16695-1.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/downloadrv_bv8rSZE.jpg"
+    },
+    {
+        id: 2,
+        type: 'product',
+        title: "(3 PACK)Old Monk Very Old Vatted XXX Rum - 750ml",
+        image: "https://i.ibb.co/jvRjVvz5/20250629-120031.jpg",
+        discount: "75% Off",
+        price: "₹5999",
+        additionalPrice: "₹399.00",
+        sliderImages: [
+            "https://i.ibb.co/jvRjVvz5/20250629-120031.jpg",
+        
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/rv.jpg"
+    },
+     {
+        id: 26,
+        type: 'product',
+        title: "Moet & Chandon Ice Imp 75cl",
+        image: "https://i.ibb.co/xtf8CDpB/1-02-J01031-MC-ICE-IMP-75-CL.png",
+        discount: "75% Off",
+        price: "₹5999",
+        additionalPrice: "₹329.00",
+        sliderImages: [
+            "https://i.ibb.co/xtf8CDpB/1-02-J01031-MC-ICE-IMP-75-CL.png",
+        
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/rv.jpg"
+    },
+    {
+        id: 23,
+        type: 'product',
+        title: "Chivas Regal 18 YO 100cl",
+        image: "https://i.ibb.co/zWTrqBXR/1-02-N01031.jpg",
+        discount: "78% Off",
+        price: "11999",
+        additionalPrice: "449.00",
+        sliderImages: [
+            "https://i.ibb.co/zWTrqBXR/1-02-N01031.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/singham.jpg"
+    },
+    {
+        id: 24,
+        type: 'product',
+        title: "(3 PACK)Ballantine's 12YO 100cl",
+        image: "https://i.ibb.co/N2rSH6HR/20250709-002642.jpg",
+        discount: "78% Off",
+        price: "11999",
+        additionalPrice: "489.00",
+        sliderImages: [
+            "https://i.ibb.co/N2rSH6HR/20250709-002642.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/singham.jpg"
+    },
+    {
+        id: 25,
+        type: 'product',
+        title: "(5 PACK)McDowell's No.1 Platinum Luxury Whisky (750ml",
+        image: "https://i.ibb.co/sJC42yxw/20250709-001728.jpg",
+        discount: "75% Off",
+        price: "₹5999",
+        additionalPrice: "₹398.00",
+        sliderImages: [
+            "https://i.ibb.co/sJC42yxw/20250709-001728.jpg",
+        
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/rv.jpg"
+    },
+    {
+        id: 3,
+        type: 'product',
+        title: "(3 PACK)Royal Salute 21 Year Old Blended Scotch Whisky.",
+        image: "https://i.ibb.co/0RhcJDqf/20250629-114559.jpg",
+        discount: "78% Off",
+        price: "11999",
+        additionalPrice: "699.00",
+        sliderImages: [
+            "https://i.ibb.co/0RhcJDqf/20250629-114559.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/singham.jpg"
+    },
+    {
+        id: 4,
+        type: 'product',
+        title: "(4 PACK) in Stock Jameson Irish Whiskey",
+        image: "https://i.ibb.co/rGdGML0z/20250629-121017.jpg",
+        discount: "73% Off",
+        price: "₹2899",
+        additionalPrice: "₹519.00",
+        sliderImages: [
+            "https://i.ibb.co/rGdGML0z/20250629-121017.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/download_rMYhJP3.jpg"
+    },
+    {
+        id: 5,
+        type: 'product',
+        title: "(5 PACK)Royal Challenge Premium Gold Whisky ",
+        image: "https://i.ibb.co/3m2nr7XV/20250629-115343.jpg",
+        discount: "76% Off",
+        price: "₹6999",
+        additionalPrice: "799.00",
+        sliderImages: [
+            "https://i.ibb.co/3m2nr7XV/20250629-115343.jpg",
+            
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/download_MSKov80.jpg"
+    },
+    {
+        id: 6,
+        type: 'product',
+        title: "(12 PACK) Kingfisher Extra Strong Premium Beer",
+        image: "https://i.ibb.co/5WLQ20N7/12bottle.webp",
+        discount: "77% Off",
+        price: "₹3999",
+        additionalPrice: "₹499.00",
+        sliderImages: [
+            "https://i.ibb.co/5WLQ20N7/12bottle.webp",
+            
+
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/download_Xj2qTgj.jpg"
+    },
+    {
+        id: 7,
+        type: 'product',
+        title: "Black dog Whiskey",
+        image: "https://i.ibb.co/ZR0tpLbw/2aaf4a77ed850e5b30ff4a6ebd49da93.jpg",
+        discount: "78% Off",
+        price: "₹1599",
+        additionalPrice: "₹189.00",
+        sliderImages: [
+            "https://i.ibb.co/ZR0tpLbw/2aaf4a77ed850e5b30ff4a6ebd49da93.jpg",
+            
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/comimg_mdobJnO.jpg"
+    },
+    {
+        id: 8,
+        type: 'product',
+        title: "(6 PACK)Johnnie Walker Double Black",
+        image: "https://i.ibb.co/0Rn2VkfH/20250629-120743.jpg",
+        discount: "99% Off",
+        price: "₹11999",
+        additionalPrice: "₹999.00",
+        sliderImages: [
+            "https://i.ibb.co/0Rn2VkfH/20250629-120743.jpg",
+            
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/sdfgsdfgsdfgsdfg.jpg"
+    },
+    {
+        id: 9,
+        type: 'product',
+        title: "Jack Daniel's Old No. 7 Tennessee Sour Mash Whiskey",
+        image: "https://i.pinimg.com/736x/37/00/e6/3700e6b417d1c329a8cc2bb918918a97.jpg",
+        discount: "76% Off",
+        price: "₹1899",
+        additionalPrice: "219.00",
+        sliderImages: [
+            "https://i.pinimg.com/736x/37/00/e6/3700e6b417d1c329a8cc2bb918918a97.jpg",
+            "https://delhidutyfree.co.in/media/catalog/product/cache/6cbf6d7749c3250aa73fcff36dbc6cfe/2/0/2000210.webp",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/egergwertgqertgwertg.jpg"
+    },
+    {
+        id: 10,
+        type: 'product',
+        title: "(3PACK) McDowell's No.1 Celebration Deluxe XXX Rum",
+        image: "https://i.ibb.co/SDWb4Cbk/20250629-113423.jpg",
+        discount: "76% Off",
+        price: "₹2999",
+        additionalPrice: "349.00",
+        sliderImages: [
+            "https://i.ibb.co/SDWb4Cbk/20250629-113423.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/asoldjfhaosdfoasdjfkl.jpg"
+    },
+    {
+        id: 11,
+        type: 'product',
+        title: "(3PACK)Label 5 Blended Scotch Whisky - Classic Black",
+        image: "https://i.ibb.co/wN7yp7nX/20250708-232756.jpg",
+        discount: "80% Off",
+        price: "₹5999",
+        additionalPrice: "379.00",
+        sliderImages: [
+            "https://i.ibb.co/wN7yp7nX/20250708-232756.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/asdfgwerfghweqrtgyqertqertqwert.jpg"
+    },
+    {
+        id: 12,
+        type: 'product',
+        title: "(4 PACK)Seagram's Imperial Blue Superior Grain Whisky",
+        image: "https://i.ibb.co/d0jYRD3w/20250708-233046.jpg",
+        discount: "80% Off",
+        price: "₹2799",
+        additionalPrice: "₹319.00",
+        sliderImages: [
+            "https://i.ibb.co/d0jYRD3w/20250708-233046.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/jgtyujrtyhj.jpg"
+    },
+    {
+        id: 13,
+        type: 'product',
+        title: "(5 PACK)Royal Stag Barrel Select Whisky",
+        image: "https://i.ibb.co/GmJqc6X/20250708-232413.jpg",
+        discount: "75% Off",
+        price: "₹4599",
+        additionalPrice: "₹359.00",
+        sliderImages: [
+            "https://i.ibb.co/GmJqc6X/20250708-232413.jpg",
+            
+        ], info: "https://rc.super-deel.site/uffr/media/polls/images/fasdfasdfasdf.jpg"
+    },
+    {
+        id: 14,
+        type: 'product',
+        title: "Phillies Chocolate Flavored Cigar",
+        image: "https://i.ibb.co/chPf2ZLT/1750875783-56-Jqjuv-M-800-800.webp",
+        discount: "74% Off",
+        price: "₹890",
+        additionalPrice: "₹159.00",
+        sliderImages: [
+            "https://i.ibb.co/chPf2ZLT/1750875783-56-Jqjuv-M-800-800.webp",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/asdfgawedtgqerg.jpg"
+    },
+    {
+        id: 15,
+        type: 'product',
+        title: "Budweiser",
+        image: "https://i.ibb.co/hzxddtv/20250708-232148.jpg",
+        discount: "76% Off",
+        price: "₹1899,",
+        additionalPrice: "₹289.00",
+        sliderImages: [
+            "https://i.ibb.co/hzxddtv/20250708-232148.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/5.jpg"
+    },
+    {
+        id: 16,
+        type: 'product',
+        title: "Turborg - kingfisher premium strong - hayward 2000",
+        image: "https://i.ibb.co/0VQhfMLH/1000015887-removebg-preview.png",
+        discount: "78% Off",
+        price: "₹1999",
+        additionalPrice: "₹259.00",
+        sliderImages: [
+            "https://i.ibb.co/0VQhfMLH/1000015887-removebg-preview.png",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/5_3kjoVph.jpg"
+    },
+    {
+        id: 17,
+        type: 'product',
+        title: "MAGIC MOMENTS 500ML",
+        image: "https://mansionz.in/cdn/shop/files/MAGICMOMENTSORANGE.jpg?v=1715090281",
+        discount: "78% Off",
+        price: "₹1400   ",
+        additionalPrice: "₹198.00",
+        sliderImages: [
+            "https://mansionz.in/cdn/shop/files/MAGICMOMENTSORANGE.jpg?v=1715090281",
+            "https://mansionz.in/cdn/shop/files/MAGICMOMENTSORANGE.jpg?v=1715090281",
+        
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/5_3kjoVph.jpg"
+    },
+    {
+        id: 18,
+        type: 'product',
+        title: "(3 PACK)AMARA PINK 750 ML",
+        image: "https://i.ibb.co/zWZzqGsy/20250708-233914.jpg",
+        discount: "81% Off",
+        price: "₹1819",
+        additionalPrice: "₹339.00",
+        sliderImages: [
+            "https://i.ibb.co/zWZzqGsy/20250708-233914.jpg",
+            "https://i.ibb.co/zWZzqGsy/20250708-233914.jpg",
+            
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/5_JxoDcj6.jpg"
+    },
+    {
+        id: 21,
+        type: 'product',
+        title: "(4 PACK)MAGIC MOMENTS CHOCOLATE 750ML",
+        image: "https://i.ibb.co/gZc09t1J/20250708-233631.jpg",
+        discount: "84% Off",
+        price: "₹4999",
+        additionalPrice: "₹389.00",
+        sliderImages: [
+            "https://i.ibb.co/gZc09t1J/20250708-233631.jpg",
+           
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/5_hxwqr2O.jpg"
+    },
+    {
+        id: 22,
+        type: 'product',
+        title: "Absolut Vodka SWEDISH VODKA",
+        image: "https://www.absolut.com/cdn-cgi/image/fit=cover,format=auto,height=414,quality=55,width=414/wp-content/uploads/product_absolut-vodka_atlas_global_1x1.jpg",
+        discount: "84% Off",
+        price: "₹2999",
+        additionalPrice: "₹249.00",
+        sliderImages: [
+            "https://www.absolut.com/cdn-cgi/image/fit=cover,format=auto,height=414,quality=55,width=414/wp-content/uploads/product_absolut-vodka_atlas_global_1x1.jpg",
+        
+        ],
+        info: "https://rc.super-deel.site/uffr/media/polls/images/5_hxwqr2O.jpg"
+    },
+    
+];
